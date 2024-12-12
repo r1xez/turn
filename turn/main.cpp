9d1d9199c130496968aee2d0a4fd9bc6bd2072de
r1xez <164931719+r1xez@users.noreply.github.com>
2024-12-12 21:36:56 +0200

    not complted

--- turn/main.cpp
+++ turn/main.cpp
@@ -1,8 +1,58 @@
-#include <iostream>;
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <ctime>
+#include <string>
+
 using namespace std;
 
-int main()
-{
+struct PrintRequest {
+    string clientName;
+    int priority;
+    time_t timestamp;
 
-	return 0;
+    PrintRequest(string name, int prio)
+        : clientName(name), priority(prio), timestamp(std::time(nullptr)) {}
+
+    bool operator<(const PrintRequest& other) const {
+        return priority < other.priority;
+    }
+};
+
+
+void printStats(const vector<PrintRequest>& printLog) {
+    cout << "Printing Statistics:" << endl;
+    for (const auto& req : printLog) {
+        cout << "Client: " << req.clientName
+            << ", Priority: " << req.priority
+            << ", Timestamp: " << std::ctime(&req.timestamp);
+    }
+}
+
+int main() {
+    priority_queue<PrintRequest> printQueue;
+    vector<PrintRequest> printLog;
+
+  
+    printQueue.push(PrintRequest("Client1", 2));
+    printQueue.push(PrintRequest("Client2", 1));
+    printQueue.push(PrintRequest("Client3", 3));
+
+    // ƒрукуЇмо запити в черз≥
+    while (!printQueue.empty()) {
+        PrintRequest currentRequest = printQueue.top();
+        printQueue.pop();
+
+       
+        printLog.push_back(currentRequest);
+
+        cout << "Printing request from " << currentRequest.clientName
+            << " with priority " << currentRequest.priority
+            << " at " << std::ctime(&currentRequest.timestamp);
+    }
+
+    
+    printStats(printLog);
+
+    return 0;
 }
